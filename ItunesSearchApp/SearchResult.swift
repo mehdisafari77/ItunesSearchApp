b17fc63a58ff73c4033b2795f056dafbded71641
Mehdi Safari <75599021+mehdisafari77@users.noreply.github.com>
2022-03-07 20:11:58 -0800

    serachResult

---
+++ ItunesSearchApp/SearchResult.swift
@@ -0,0 +1,23 @@
+//
+//  SearchResult.swift
+//  ItunesSearchApp
+//
+//  Created by Mehdi MMS on 07/03/2022.
+//
+
+import Foundation
+
+class SearchResult {
+    var artistName = ""
+    var trackName = ""
+    var itemDescription = ""
+    var trackPrice = 0.00
+    
+    init?(json: [String : AnyObject ]) {
+        guard let artistName = json["artistName"] as? String,
+              let trackName = json["trackName"] as? String else { return nil }
+        
+        let itemDescription = json["itemDescription"]
+        let price = json["trackPrice"]
+    }
+}
